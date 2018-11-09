d16d818d4acddf70743fd9186c225068f9476f3d
SasquatchXYZ <dalton.ricker.2011@graduate.usmma.edu>
2018-11-09 12:33:11 -0500

    Testing...

---
+++ server.js
@@ -0,0 +1,32 @@
+// Module Dependencies
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Establishes the Express Server & Port
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Sets up Express to handle data parsing for urlencoded or json formatting types
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Configures the Routes for the Server to use
+
+require('./app/routing/apiRoutes')(app);
+require('./app/routing/htmlRoutes')(app);
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Starts the Server
+
+app.listen(PORT, function () {
+    console.log(`App listening on http://localhost:${PORT}`);
+    //console.log(`App listening on PORT: ${PORT}`);
+});
+
